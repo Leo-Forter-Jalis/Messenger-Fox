8aa747dbcefc7a9d92fcd374093f862633317b67
Leo-Forter-Jalis <leonarld_work@mail.ru>
2026-02-07 22:19:54 +0300

    refactor(protocol)

--- Protocol/src/test/java/com/lfj/messenger/protocol/test/ProtocolTest.java
+++ Protocol/src/test/java/com/lfj/messenger/protocol/test/ProtocolTest.java
@@ -19,15 +19,15 @@ public class ProtocolTest {
     public void test(){
         assertDoesNotThrow(() ->{
             new RegisterRequest(UUID.randomUUID(), "display", "username", "lol@gmail.com", "uuu", Instant.now());
-            new RegisterResponse(UUID.randomUUID(), new UserDTO(UUID.randomUUID(), "userName", "uuu", "lol@gmail.com", Instant.now()), true, Instant.now());
+            new RegisterResponse(UUID.randomUUID(), new UserDTO(UUID.randomUUID(), "userName", "uuu", Instant.now()), true, Instant.now());
             new AuthRequest(UUID.randomUUID(),"lol@gmail.com", "uuu", Instant.now());
             new AuthResponse(UUID.randomUUID(), null, false, Instant.now());
             new GetMessageRequest(UUID.randomUUID(), Instant.now());
             new GetMessageResponse(UUID.randomUUID(), Instant.now());
             new ChatsRequest(UUID.randomUUID(), Instant.now());
             new ChatsResponse(UUID.randomUUID(), List.of(), Instant.now());
-            new MessageRequest(UUID.randomUUID(), new MessageDTO(UUID.randomUUID(), null, UUID.randomUUID(), UUID.randomUUID(), MessageType.TEXT.name(), "", Instant.now()),Instant.now());
-            new MessageResponse(UUID.randomUUID(), new MessageDTO(UUID.randomUUID(), null, UUID.randomUUID(), UUID.randomUUID(), MessageType.TEXT.name(), "АХАХАХАХХА", null), Instant.now());
+            new MessageRequest(UUID.randomUUID(), new MessageDTO(UUID.randomUUID(), null, new UserDTO(UUID.randomUUID(), "TEST", "TEST", Instant.now()), UUID.randomUUID(), MessageType.TEXT.name(), "", Instant.now()),Instant.now());
+            new MessageResponse(UUID.randomUUID(), new MessageDTO(UUID.randomUUID(), null, new UserDTO(UUID.randomUUID(), "TEST", "TEST", Instant.now()), UUID.randomUUID(), MessageType.TEXT.name(), "АХАХАХАХХА", null), Instant.now());
         });
     }
     @Test
