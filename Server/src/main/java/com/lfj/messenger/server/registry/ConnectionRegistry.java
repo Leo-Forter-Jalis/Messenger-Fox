7512ee288150ba7751a369f9fcadf860900da3ce
Leo-Forter-Jalis <leonarld_work@mail.ru>
2026-02-20 19:16:50 +0300

    feat(server) refactoring

--- Server/src/main/java/com/lfj/messenger/server/registry/ConnectionRegistry.java
+++ Server/src/main/java/com/lfj/messenger/server/registry/ConnectionRegistry.java
@@ -1,4 +1,4 @@
-package com.lfj.messenger.server;
+package com.lfj.messenger.server.registry;
 
 import io.netty.channel.Channel;
 
@@ -8,15 +8,10 @@ import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionRegistry {
-    private Map<UUID, Channel> connections = new ConcurrentHashMap<>();
+    private final Map<UUID, Channel> connections = new ConcurrentHashMap<>();
 
     public void add(UUID uuid, Channel channel) { this.connections.put(uuid, channel); }
     public void remove (UUID uuid) { this.connections.remove(uuid); }
     public Optional<Channel> get(UUID uuid) { return  Optional.ofNullable(this.connections.get(uuid)); }
     public boolean isOnline(UUID uuid){ return this.connections.containsKey(uuid); }
-    public void display(){
-        for(UUID uuid : connections.keySet()){
-            System.out.println(uuid.toString());
-        }
-    }
 }
