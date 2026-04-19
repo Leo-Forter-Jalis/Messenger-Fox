8577ede4fb8aa8e7a33eaae8ee6d5828a422e4a7
Leo-Forter-Jalis <leonarld_work@mail.ru>
2026-04-19 21:26:34 +0300

    feat(server) move file

--- Server/src/main/kotlin/com/lfj/messfox/server/Util.kt
+++ Server/src/main/kotlin/com/lfj/messfox/server/Util.kt
@@ -1,8 +1,8 @@
-package com.lfj.messfox.server.dao
+package com.lfj.messfox.server
 
-import java.util.*
+import java.util.Optional
 
 inline fun <T, R> Optional<T>.ifPresentOrElseWithResult(crossinline action: (T) -> R, crossinline emptyAction: () -> R): R{
         return if(isPresent) action(get())
         else emptyAction()
- }
+}
