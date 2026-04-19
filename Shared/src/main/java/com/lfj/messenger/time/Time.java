1dd46c81e2ab9c16e64eeb8933b32b5d457bf7ec
Leo-Forter-Jalis <leonarld_work@mail.ru>
2026-04-19 21:42:46 +0300

    feat(shared) update Time

--- Shared/src/main/java/com/lfj/messenger/time/Time.java
+++ Shared/src/main/java/com/lfj/messenger/time/Time.java
@@ -8,14 +8,15 @@ import java.util.Locale;
 
 public final class Time {
     private final static ZoneId TIME_ZONE = ZoneId.systemDefault();
-    private final static DateTimeFormatter UI_FORMAT = DateTimeFormatter
-            .ofLocalizedDate(FormatStyle.MEDIUM)
-            .withLocale(Locale.getDefault())
-            .ofPattern("yyyy-MM-dd | HH:mm:ss");
+    private static DateTimeFormatter UI_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd | HH:mm:ss").withZone(ZoneId.systemDefault());
+    private static DateTimeFormatter UI_DAY_AND_MONTH_FORMAT = DateTimeFormatter.ofPattern("dd MMMM").withZone(ZoneId.systemDefault());
     private Time(){
     }
     public static String getTime(Instant instant){
         return instant.atZone(TIME_ZONE).format(UI_FORMAT);
+    }
+    public static String getDayAndMonthTime(Instant instant){
+        return instant.atZone(TIME_ZONE).format(UI_DAY_AND_MONTH_FORMAT);
     }
     public static Instant nowInstant(){return Instant.now();}
 }
